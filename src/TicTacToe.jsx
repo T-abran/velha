b6cf96ba14c7354346fd55903dd20317b17240ec
Tiago Abranges <tiagoabranges@gmail.com>
2022-01-03 19:54:36 -0300

    jogo da velha

---
+++ src/TicTacToe.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './TicTacToe.css';
+
+function TicTacToe() {
+  const emptyBoard = Array(9).fill("");
+  const [board, setboard] = useState(emptyBoard);
+
+  const handleCellClick = (index) => {
+    setboard(board.map((item, itemIndex) => itemIndex === index ? "X" : item));
+  }
+  return (
+    <main>
+       <h1 className='title' >jogo da velha</h1>
+    <div className='board'>
+   {board.map((item, index) => (
+      <div
+     key={index}
+       className= {`cell ${item}`}
+       onClick={() => handleCellClick(index)}
+       >
+         {item}
+     </div>
+   ))}
+    </div>
+    </main>
+  );
+}
+
+export default TicTacToe;
